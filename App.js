c15c37062417e8316734d2bbbae1000a44c91251
Zafer AYAN <ozcanzaferayan@gmail.com>
2023-11-23 20:19:08 +0300

    Add Pikachu and listing pokemons

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  FlatList,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
+const BASE_URL = "https://my-json-server.typicode.com/ozcanzaferayan/pokedex";
+const BASE_IMAGE_URL =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home";
 
 export default function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/pokemons`)
+      .then((res) => res.json())
+      .then((data) => setPokemons(data));
+  }, []);
+
+  console.log(pokemons);
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      <Text>Hello World</Text>
-    </View>
+      <FlatList
+        data={pokemons}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Image
+              style={{ width: 64, height: 64 }}
+              source={{ uri: `${BASE_IMAGE_URL}/${item.pokemonId}.png` }}
+            />
+            <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+              {item.name}
+            </Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
