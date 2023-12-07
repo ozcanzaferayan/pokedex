df650333b83b516fdb8721a02b0d89c43e563819
Zafer AYAN <ozcanzaferayan@gmail.com>
2023-12-08 00:12:06 +0300

    Add 3-week-navigation

--- App.js
+++ App.js
@@ -3,11 +3,40 @@ import React from "react";
 import HomeScreen from "./src/features/HomeScreen";
 import { Provider } from "react-redux";
 import store from "./src/app/store";
+import { NavigationContainer } from "@react-navigation/native";
+import SettingsScreen from "./src/features/SettingsScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              tabBarLabel: "Anasayfa",
+              tabBarIcon: ({ color, size }) => (
+                <Feather name="home" size={size} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={SettingsScreen}
+            options={{
+              tabBarLabel: "Ayarlar",
+              tabBarIcon: ({ color, size }) => (
+                <Feather name="settings" size={size} color={color} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 };
